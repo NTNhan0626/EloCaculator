32fc013ce2b7157b12a381b1ad04fb807032225c
NTNhan0626 <ntnhan0626@gmail.com>
2025-05-08 23:47:15 +0700

    generate player

--- src/services/generateData.js
+++ src/services/generateData.js
@@ -1,4 +1,16 @@
 const Player = require('../models/player')
 const Matche = require('../models/matche')
 
-function 
+function generaPlayer(number){
+    const players = [];
+    for(let i =0 ;i<number;i++){
+        players.push(new Player(i,"pl"+ i+1,1000,[]))
+    }
+    return players;
+}
+
+const players = generaPlayer(100);
+
+const player = players[10];
+player.updateElo(20);
+console.log(players);
